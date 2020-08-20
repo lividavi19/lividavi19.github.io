18face29f0e1d24d76bd893c3b65f9557621cfe0
Livingstone <43893458+lividavi19@users.noreply.github.com>
2020-08-20 13:10:37 +0300

    Added a service worker file

---
+++ sw.js
@@ -0,0 +1,40 @@
+const SW_VERSION = 0;
+const STATIC_CACHE_NAME = `static://lividavi19.github.io-${SW_VERSION}`;
+const APP_SHELL = [
+	`.`,
+	`index.html`,
+	`css/base.css`,
+	`js/base.js`,
+	`img/icons/ic_192.png`,
+	`img/icons/ic_512.png`
+];
+
+// install event
+self.oninstall = e => {
+	e.waitUntil(
+		caches.open(STATIC_CACHE_NAME).then(cache => {
+			cache.addAll(APP_SHELL);
+		})
+	);
+};
+
+// activate event
+self.onactivate = e => {
+	e.waitUntil(
+		caches.keys().then(keys => {
+			return Promise.all(keys
+				.filter(key => key !== STATIC_CACHE_NAME)
+				.map(key => caches.delete(key))
+			);
+		})
+	);
+};
+
+// fetch event
+self.onfetch = e => {
+	e.respondWith(
+		caches.match(e.request).then(cached_response => {
+			return cached_response || fetch(e.request);
+		})
+	);
+};
