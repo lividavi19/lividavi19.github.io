6d6bc09a105b49e509b46f9c9552268fcfbea43b
Livingstone <43893458+lividavi19@users.noreply.github.com>
2022-01-17 16:18:18 +0300

    Delete js directory

--- js/base.js
+++
@@ -1,7 +0,0 @@
-// get element(s) reference 
-function getNode(e) {return document.querySelector(e);}
-function getNodes(e) {return document.querySelectorAll(e);}
-// log on the console
-function consoleLog (e) {console.log(e)}
-function consoleWarn (e) {console.warn(e)}
-function consoleError (e) {console.error(e)}
